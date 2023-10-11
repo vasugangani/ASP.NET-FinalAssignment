320a8847657833c3d57fa52a92739d3fbc76c44e
Vasu Gangani <vasugangani4352@gmail.com>
2023-10-10 22:27:33 -0400

    Models added

---
+++ MakeMyTripTravelPlan/MakeMyTripTravelPlan/Models/AirlineService.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MakeMyTripTravelPlan.Models
+{
+    public class AirlineService
+    {
+        [Required]
+
+        public int AirlineSeriveId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+
+        public string Description { get; set; }
+        [Required]
+
+        public string Avaialable { get; set; }
+
+        //Child reference
+        public List<Flight>? Flights { get; set; }
+    }
+}
---
+++ MakeMyTripTravelPlan/MakeMyTripTravelPlan/Models/Flight.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MakeMyTripTravelPlan.Models
+{
+    public class Flight
+    {
+        [Required]
+
+
+        public string FlightDeatil { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        [Required]
+
+        public DateTime Date { get; set; }
+        [Required]
+
+        public string Description { get; set; }
+
+        public string AirlineServiceId { get; set; }
+
+        // Parent Reference - connection to 1 airline
+        public AirlineService? AirlineService { get; set; }
+
+    }
+}
